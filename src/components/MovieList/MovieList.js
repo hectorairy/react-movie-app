449a4eb4f24c0303034de5ae9da0bc35896a5025
HectorAJimHer97 <h.jimenez@chimera.mx>
2020-02-27 17:59:50 -0600

    Terminado

---
+++ src/components/MovieList/MovieList.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { List, Avatar, Button } from "antd";
+import { Link } from "react-router-dom";
+import Loading from "../Loading";
+
+import "./MovieList.scss";
+
+export default function MovieList(props) {
+  const { movies, title } = props;
+
+  if (movies.loading && !movies.return) {
+    return <Loading />;
+  }
+
+  return (
+    <List
+      className="movieList"
+      size="default"
+      header={<h2>{title}</h2>}
+      bordered
+      dataSource={movies.result.results}
+      renderItem={movie => <RenderMovie movie={movie} />}
+    />
+  );
+}
+
+function RenderMovie(props) {
+  const {
+    movie: { id, title, poster_path }
+  } = props;
+  const posterPath = `https://image.tmdb.org/t/p/original${poster_path}`;
+
+  return (
+    <List.Item className="movieList__movie">
+      <List.Item.Meta
+        avatar={<Avatar src={posterPath} />}
+        title={<Link to={`/movie/${id}`}>{title}</Link>}
+      />
+      <Link to={`/movie/${id}`}>
+        <Button type="primary" shape="circle" icon="right"></Button>
+      </Link>
+    </List.Item>
+  );
+}
---
+++ src/utils/constants.js
@@ -0,0 +1,3 @@
+export const URL_API = "https://api.themoviedb.org/3";
+
+export const API = "d98463a55914a637b851114950db1a77";
